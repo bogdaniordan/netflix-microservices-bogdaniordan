8cd77a536e5d62411119464451548e88022a2b2c
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-08-02 15:26:38 +0300

    Removed spring security

--- video-api-gateway-spring/cloud-gateway/src/main/java/com/codecool/cloudgateway/CloudGatewayApplication.java
+++ video-api-gateway-spring/cloud-gateway/src/main/java/com/codecool/cloudgateway/CloudGatewayApplication.java
@@ -1,36 +1,39 @@
 package com.codecool.cloudgateway;
 
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import com.codecool.cloudgateway.model.DbUser;
-import com.codecool.cloudgateway.repository.UserRepository;
+//import com.codecool.cloudgateway.model.DbUser;
+//import com.codecool.cloudgateway.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import java.util.List;
 
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableZuulProxy
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner initDatabase(UserRepository userRepository) {
-		return args -> {
-			DbUser dbUser = DbUser.builder().id(1L)
-							.username("bogdan").password(BCrypt.hashpw("ilovejs", BCrypt.gensalt(12)))
-					.roles(List.of("ROLE_USER")).build();
-			userRepository.save(dbUser);
-//			userRepository.save(new DbUser(1L, "bogdan", (BCrypt.hashpw("ilovejs"), BCrypt.gensalt(12)), List.of("ROLE_USER", "ADMIN_USER")));
-		};
-	}
+//	@Bean
+//	CommandLineRunner initDatabase(UserRepository userRepository) {
+//		return args -> {
+//			DbUser dbUser = DbUser.builder().id(1L)
+//							.username("bogdan").password(BCrypt.hashpw("ilovejs", BCrypt.gensalt(12)))
+//					.roles(List.of("ROLE_USER")).build();
+//			userRepository.save(dbUser);
+////			userRepository.save(new DbUser(1L, "bogdan", (BCrypt.hashpw("ilovejs"), BCrypt.gensalt(12)), List.of("ROLE_USER", "ADMIN_USER")));
+//		};
+//	}
 
 }
